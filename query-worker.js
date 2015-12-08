374b4dd2f466517eff82e98bc6a728acf59ac607
pschoeps <schoeps.pat@gmail.com>
2015-12-08 00:45:45 +0000

    ok

--- query-worker.js
+++ query-worker.js
@@ -1,11 +1,28 @@
 
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var socketio = require('socket.io');
+var express = require('express');
+
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
 var Queue = require('firebase-queue'),
     Firebase = require('firebase');
 
 var ref = new Firebase('https://urvirl.firebaseio.com/queue');
 var queue = new Queue(ref, function(data, progress, resolve, reject) {
   // Read and process task data
-  console.log(data);
+  console.log(data.message);
 
   // Do some work
   progress(50);
