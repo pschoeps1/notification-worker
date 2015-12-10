d3d21d349e97e8f9f3087e744b8c6946ae1f96bd
pschoeps <schoeps.pat@gmail.com>
2015-12-10 22:28:49 +0000

    ok

--- query-worker.js
+++ query-worker.js
@@ -22,14 +22,44 @@ var Queue = require('firebase-queue'),
 var ref = new Firebase('https://urvirl.firebaseio.com');
 var queueRef = ref.child('queue');
 var messagesRef = ref.child('chat/room-messages');
+var userRef = ref.child('chat/users');
+var url = 'mighty-mesa-2159.herokuapp.com'
 
-var options = {
-  'specId': 'sanitize_message'
+
+
+var Queue = new Queue(queueRef, function(data, progress, resolve, reject) {
+  // sanitize input message
+  console.log(data);
+  
+ var options = {
+  host: url,
+  port: 80,
+  path: parse('/v1/users/'+ data.userId +'/notifications?chat_id=%s', data.chat_room),
+  method: 'GET'
 };
 
-var sanitizeQueue = new Queue(queueRef, options, function(data, progress, resolve, reject) {
-  // sanitize input message
-  console.log(data.message);
+http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+}).end();
+
+queueRef.child("tasks").child(data.signature).remove();
+
+
+
+function parse(str) {
+    var args = [].slice.call(arguments, 1),
+        i = 0;
+
+    return str.replace(/%s/g, function() {
+        return args[i++];
+    });
+}
+
 
   // pass sanitized message and username along to be fanned out
 
